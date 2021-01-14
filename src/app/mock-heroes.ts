4b1e079dbc92702b1e7ad1c3e3bc370714d2f605
tomek <sobeczki@gmail.com>
2021-01-14 18:52:29 +0000

    part II

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+
+    { id: 11, name: 'Thomas'},
+    { id: 12, name: 'Peter'},
+    { id: 13, name: 'Agnes'},
+    { id: 14, name: 'Kris'}
+
+];
