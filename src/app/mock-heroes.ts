b4ae5588689508d346c375e53ab9a8287753f370
tom <sobeczki@gmail.com>
2021-01-14 20:07:10 +0000

    add picture

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,9 +2,10 @@ import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
 
-    { id: 11, name: 'Thomas'},
-    { id: 12, name: 'Peter'},
-    { id: 13, name: 'Agnes'},
-    { id: 14, name: 'Kris'}
+    { id: 11, name: 'Thomas', address: 'Mallow'},
+    { id: 12, name: 'Peter' , address: 'Cork'},
+    { id: 13, name: 'Agnes' , address: 'Limerick'},
+    { id: 14, name: 'Kris' , address: 'Namyslow'},
+    { id: 15, name: 'Janek', address: 'Smogorzow'}
 
 ];
